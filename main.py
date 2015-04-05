d89e4bd7974dfbe47de66ae96eecf17a97fbebf8
Suruga Devil <epierce118@gmail.com>
2015-04-05 12:06:24 -0400

    Work #1

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-# This is the program that we are currently testing
-#my_input = 5 # setting input
-#x = my_input + 1 # math here
-#print x # print result
+# # # This is the program that we are currently testing
+# # # Current goal- find most recent update on a Github repository
+print("Welcome to Hydraga! Type 'help' for a list of commands.\n")
 
-print("Welcome to Hydraga")
+# importing necessary libraries
+from bs4 import BeautifulSoup
+import requests
+import re
 
-import urllib.request
+url = input('Enter your URL here: ')
 
-url = urllib.request.urlopen('https://www.github.com/htll')
+try:
+    url = url.strip()
+except:
+    pass
 
-print(url.read())
+
+# cleaning input
+
+if url.startswith("http") == 'True':
+    r = requests.get(url)
+elif url.startswith("https") == "True":
+    r = requests.get(url)
+else:
+    r = requests.get('http://' +url)
+
+# using beautiful soup to obtain source of page
+data = r.text
+soup = BeautifulSoup(data)
+result = (soup.prettify())
+
+#print(result)
+
+# using regex to scrape datetime from result
+p = re.compile(r'datetime=\"(.*?)T(.*?)Z\"')
+t = re.search(p, result)
+result = t.group(1)
+result2 = t.group(2)
+print("The last time this bitch was edited was: %s %s" % (result,result2))
